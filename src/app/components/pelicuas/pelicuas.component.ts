527cc9f1b76755ac39c8395ba5ce0da3f61feb8e
Baruc Ramirez <barux111706@gmail.com>
2026-03-11 09:59:49 -0600

    actualizacion

--- src/app/components/pelicuas/pelicuas.component.ts
+++ src/app/components/pelicuas/pelicuas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pelicula } from '../models/pelicula';
 
 @Component({
   selector: 'app-pelicuas',
@@ -8,26 +9,26 @@ import { Component } from '@angular/core';
 })
 export class PelicuasComponent {
   public titulo: string;
-  public peliculas: Array<any>;
+  public peliculas: Array<Pelicula>;
 
   constructor(){
     this.titulo = "Componente de Peliculas";
     this.peliculas = [
-      {
-        title: "Avengers Endgame",
-        image: "https://i.pinimg.com/736x/67/91/b3/6791b331f134fa878b6c66e9b9d833c2.jpg",
-        description: "Los Vengadores restantes intentan revertir el daño causado por Thanos."
-      },
-      {
-        title: "Spiderman No Way Home",
-        image: "https://i.redd.it/0z22ocvl3dy71.jpg",
-        description: "Peter Parker enfrenta el caos del multiverso después de que su identidad es revelada."
-      },
-      {
-        title: "S.W.A.T.",
-        image: "https://i.pinimg.com/736x/13/9d/f7/139df72fa73d840a20320ec26e46ca4f.jpg",
-        description: "Un equipo de fuerzas especiales de la policía de Los Ángeles combate el crimen."
-      }
+      new Pelicula(
+        "Avengers Endgame",
+        "https://i.pinimg.com/736x/67/91/b3/6791b331f134fa878b6c66e9b9d833c2.jpg",
+        "Los Vengadores restantes intentan revertir el daño causado por Thanos."
+      ),
+      new Pelicula(
+        "Spiderman No Way Home",
+        "https://i.redd.it/0z22ocvl3dy71.jpg",
+        "Peter Parker enfrenta el caos del multiverso después de que su identidad es revelada."
+      ),
+      new Pelicula(
+        "S.W.A.T.",
+        "https://i.pinimg.com/736x/13/9d/f7/139df72fa73d840a20320ec26e46ca4f.jpg",
+        "Un equipo de fuerzas especiales de la policía de Los Ángeles combate el crimen."
+      )
     ];
   }
 
